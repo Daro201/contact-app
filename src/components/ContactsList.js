2eb5c0dd09f1748e73093c244c120d269e1e4662
Michał Gacek <gacek.michael@outlook.com>
2018-09-12 21:08:56 +0200

    Improvements - ContactDetails Component

--- src/components/ContactsList.js
+++ src/components/ContactsList.js
@@ -3,7 +3,7 @@ import React from 'react';
 const ContactsList = props => (
     <ul>
         {props.contacts.map(contact => (
-            <li key={contact.id}>
+            <li key={contact.id} onClick={() => props.clickContactHandler(contact)}>
                 {contact.name} {contact.surname}<br />
                 <small>{contact.phone}</small>
             </li>
